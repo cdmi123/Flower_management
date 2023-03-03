dadf15813e6e69a1241ddfcda214bc43fe78b5b2
cdmi123 <languagepdf@gmail.com>
2023-03-03 15:36:14 +0530

    ravi update

---
+++ Admin/Models/slider.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+
+namespace Flower_Management.Models
+{
+
+    public class slider
+    {
+
+
+        SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS;Database=Flower_management;User Id=sa;pwd=cdmi@3420");
+
+        public DataSet get_slider()
+        {
+            SqlCommand cmd = new SqlCommand("select * from [dbo].[slider]", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            return ds;
+        }
+    }
+}
